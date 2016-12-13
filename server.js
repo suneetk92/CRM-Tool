77b496217d7287daf2d1ed0325e20b9d5325177d
suneetk92 <suneetk92@gmail.com>
2016-12-13 21:50:39 +0530

    Adding mongodb driver

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-
 var http = require('http');
 var express = require('express');
+var mongodb = require('mongodb');
+var assert = require('assert');
+
 var app = express();
+var MongoClient = mongodb.MongoClient;
+
+var url = 'mongodb://localhost:27017/test';
+MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server.");
+    db.close();
+});
+
 app.use(express.static(__dirname+'/public'));
 app.get('/page',function(req,res){
 	res.sendFile(__dirname+'/public/view/index.html');
