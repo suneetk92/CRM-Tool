6b1ee42fde53ffe58033f9a99b64cab8f18b6774
tanoaks <akshaytan2gmail.com>
2017-01-04 21:25:27 +0530

    folder

---
+++ config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by akshay on 12/31/2016.
+ */
+module.exports = {
+    'port'     : process.env.PORT || 1337,
+    'database' : 'mongodb://localhost:27017/api',
+    'secret'   : 'thisisSecret'
+};
--- server.js
+++ server.js
@@ -2,11 +2,26 @@ var http = require('http');
 var express = require('express');
 var mongodb = require('mongodb');
 var assert = require('assert');
-
+var morgan     = require('morgan');
+var config =require('./config');
 var app = express();
+var api =require('./app/api/api')(app,express);
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 var MongoClient = mongodb.MongoClient;
 
-var url = 'mongodb://localhost:27017/test';
+var url = config.database;
+
+app.use(morgan('dev'));
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Header', 'X-Requested-With, content-type, Authorization');
+    next();
+});
+app.use('/api',api);
 MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     console.log("Connected correctly to server.");
@@ -18,5 +33,5 @@ app.get('/',function(req,res){
 	res.sendFile(__dirname+'/public/index.html');
 	});
 
-app.listen(8081);
-console.log('Server running at http://localhoost:8081');
+app.listen(config.port);
+console.log('Server running at http://localhost:'+config.port);
